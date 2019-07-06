61f82cbfac381119c3b823c3d26dc1713e558ab2
Evgeny_Skotarenko <jeny797@gmail.com>
2019-07-06 21:33:45 +0300

    created Page Object Model

--- work_folder/conftest.py
+++ work_folder/conftest.py
@@ -3,6 +3,12 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default="en",
+                     help="select language for browser")
+
+
 @pytest.fixture(scope='function')
 def browser(request):
     user_language = request.config.getoption("language")
@@ -10,8 +16,8 @@ def browser(request):
     print("\nstart browser for test..")
     options.add_experimental_option('prefs',
                                     {'intl.accept_languages': user_language})
-    browser = webdriver.Chrome(executable_path='C:\chromedriver\chromedriver.exe', options=options)
+    browser = webdriver.Chrome(
+        executable_path='C:\chromedriver\chromedriver.exe', options=options)
     yield browser
     print("\nquit browser")
     browser.quit()
-
